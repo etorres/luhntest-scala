5a98aef5ffbab6de4ac62849290964ec0557dc44
Erik Torres <erik.torres@lifullconnect.com>
2019-11-29 15:11:08 +0100

    :rocket: Done with the algorithm

--- src/main/scala/es/eriktorr/katas/LuhnValidator.scala
+++ src/main/scala/es/eriktorr/katas/LuhnValidator.scala
@@ -1,12 +1,20 @@
 package es.eriktorr.katas
 
+import es.eriktorr.katas.NumberTokenizer.digitsFrom
+
 object LuhnValidator {
 
   def isCreditCard(number: String): Boolean = {
-
-
-    // TODO
-    throw new IllegalStateException("not implemented")
+    val (oddDigits, evenDigits) = digitsFrom(number)
+      .reverse
+      .zipWithIndex
+      .partition(_._2 % 2 == 0)
+    val oddDigitsSum = oddDigits.map(_._1).sum
+    val evenDigitsSum = evenDigits
+      .map(_._1 * 2)
+      .map(d => digitsFrom(d.toString).sum)
+      .sum
+    (oddDigitsSum + evenDigitsSum) % 10 == 0
   }
 
 }
