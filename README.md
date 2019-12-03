ad35135f1afbcd7c4f03e4fb31d20d21e47769e6
Erik Torres <erik.torres@lifullconnect.com>
2019-12-03 09:47:30 +0100

    Clean-up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# luhntest-scala
+# Luhn Test kata in Scala
+
+Complete description [here](https://github.com/FunctionalKatas/Katas/tree/master/2015-01-LuhnTest).
--- src/main/scala/es/eriktorr/katas/LuhnValidator.scala
+++ src/main/scala/es/eriktorr/katas/LuhnValidator.scala
@@ -5,16 +5,38 @@ import es.eriktorr.katas.NumberTokenizer.digitsFrom
 object LuhnValidator {
 
   def isValidLuhn(number: String): Boolean = {
+    val (oddDigits, evenDigits) = reverseDigitsFrom(number)
+    totalSumEndsInZero(partialSumS1(oddDigits), partialSumS2(evenDigits))
+  }
+
+  private def partialSumS1(oddDigits: Seq[Int]): Int = {
+    oddDigits.sum
+  }
+
+  private def partialSumS2(evenDigits: Seq[Int]): Int = {
+    evenDigits.map(_ * 2)
+      .map(d => digitsFrom(d.toString).sum)
+      .sum
+  }
+
+  private def totalSumEndsInZero(s1: Int, s2: Int): Boolean = {
+    (s1 + s2) % 10 == 0
+  }
+
+  private def reverseDigitsFrom(number: String): (Seq[Int], Seq[Int]) = {
     val (oddDigits, evenDigits) = digitsFrom(number)
       .reverse
       .zipWithIndex
-      .partition(_._2 % 2 == 0)
-    val oddDigitsSum = oddDigits.map(_._1).sum
-    val evenDigitsSum = evenDigits
-      .map(_._1 * 2)
-      .map(d => digitsFrom(d.toString).sum)
-      .sum
-    (oddDigitsSum + evenDigitsSum) % 10 == 0
+      .partition {
+        case (_, index) => index % 2 == 0
+      }
+    (withoutIndexes(oddDigits), withoutIndexes(evenDigits))
+  }
+
+  private def withoutIndexes(indexedDigits: Seq[(Int, Int)]): Seq[Int] = {
+    indexedDigits.map {
+      case (value, _) => value
+    }
   }
 
 }
